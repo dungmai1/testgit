5f6869f619598e0220edac62cd461976d09277f2
FX504GE <noname12102002@gmail.com>
2022-04-28 23:02:54 +0700

    update test

---
+++ src/main/java/com/example/demo1/dice/test.java
@@ -0,0 +1,11 @@
+package com.example.demo1.dice;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Test nha");
+        testcainhe();
+    }
+    static void testcainhe(){
+        System.out.println("Test cai nhe");
+    }
+}
